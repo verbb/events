9eac370b73629d1b2604d661292cd0d010855df3
Josh Crawford <joshua@sgroup.com.au>
2024-05-22 22:43:24 +1000

    Fix event

--- src/events/PurchasedTicketEvent.php
+++ src/events/PurchasedTicketEvent.php
@@ -3,9 +3,9 @@ namespace verbb\events\events;
 
 use verbb\events\elements\PurchasedTicket;
 
-use yii\base\Event;
+use craft\events\CancelableEvent;
 
-class PurchasedTicketEvent extends Event
+class PurchasedTicketEvent extends CancelableEvent
 {
     // Properties
     // =========================================================================
