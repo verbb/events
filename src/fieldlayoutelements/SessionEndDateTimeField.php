a9eb09d11b3c8deb60c82a4033c57f52ce4ecba1
Josh Crawford <joshua@sgroup.com.au>
2024-09-30 10:00:20 +1000

    Add the ability to set the timezone on start/end dates for sessions

--- src/fieldlayoutelements/SessionEndDateTimeField.php
+++ src/fieldlayoutelements/SessionEndDateTimeField.php
@@ -1,27 +1,15 @@
 <?php
 namespace verbb\events\fieldlayoutelements;
 
-use verbb\events\elements\Session;
-
 use Craft;
 use craft\base\ElementInterface;
-use craft\fieldlayoutelements\BaseNativeField;
-use craft\helpers\Cp;
-use craft\helpers\Html;
 
-use yii\base\InvalidArgumentException;
-
-use DateTime;
-
-class SessionEndDateTimeField extends BaseNativeField
+class SessionEndDateTimeField extends SessionDateTimeField
 {
     // Properties
     // =========================================================================
 
-    public bool $required = true;
-    public bool $mandatory = true;
     public string $attribute = 'endDate';
-    public ?DateTime $defaultTime = null;
 
 
     // Protected Methods
@@ -35,52 +23,5 @@ class SessionEndDateTimeField extends BaseNativeField
     protected function defaultInstructions(?ElementInterface $element = null, bool $static = false): ?string
     {
         return Craft::t('events', 'The end date/time for the session.');
-    }
-    
-    protected function settingsHtml(): ?string
-    {
-        $html = parent::settingsHtml();
-
-        $html .= Cp::timeFieldHtml([
-            'label' => Craft::t('events', 'Default Time'),
-            'instructions' => Craft::t('events', 'Set a default time for this field for brand-new sessions.'),
-            'id' => 'default-time',
-            'name' => 'defaultTime',
-            'value' => $this->defaultTime ? $this->defaultTime->format('c') : null,
-        ]);
-
-        return $html;
-    }
-
-    protected function inputHtml(ElementInterface $element = null, bool $static = false): ?string
-    {
-        if (!$element instanceof Session) {
-            throw new InvalidArgumentException(static::class . ' can only be used in session field layouts.');
-        }
-
-        // Annoyingly, we need to construct the date/time input ourselves to have a separate date/time default value.
-        $config = [
-            'id' => 'end-date',
-            'name' => 'endDate',
-            'fieldset' => true,
-        ];
-
-        $dateConfig = $config + [
-            'hasOuterContainer' => true,
-            'isDateTime' => true,
-            'value' => $element?->endDate?->format('c') ?? null,
-        ];
-
-        $timeConfig = $config + [
-            'hasOuterContainer' => true,
-            'isDateTime' => true,
-            'outputLocaleParam' => false,
-            'outputTzParam' => false,
-            'value' => $element?->endDate?->format('c') ?? $this->defaultTime?->format('c') ?? null,
-        ];
-
-        return Cp::fieldHtml(Html::tag('div', Cp::dateHtml($dateConfig) . Cp::timeHtml($timeConfig), [
-            'class' => 'datetimewrapper',
-        ]), $config);
     }
 }
--- src/fieldlayoutelements/SessionStartDateTimeField.php
+++ src/fieldlayoutelements/SessionStartDateTimeField.php
@@ -5,23 +5,13 @@ use verbb\events\elements\Session;
 
 use Craft;
 use craft\base\ElementInterface;
-use craft\fieldlayoutelements\BaseNativeField;
-use craft\helpers\Cp;
-use craft\helpers\Html;
 
-use yii\base\InvalidArgumentException;
-
-use DateTime;
-
-class SessionStartDateTimeField extends BaseNativeField
+class SessionStartDateTimeField extends SessionDateTimeField
 {
     // Properties
     // =========================================================================
 
-    public bool $required = true;
-    public bool $mandatory = true;
     public string $attribute = 'startDate';
-    public ?DateTime $defaultTime = null;
 
 
     // Protected Methods
@@ -35,52 +25,5 @@ class SessionStartDateTimeField extends BaseNativeField
     protected function defaultInstructions(?ElementInterface $element = null, bool $static = false): ?string
     {
         return Craft::t('events', 'The start date/time for the session.');
-    }
-    
-    protected function settingsHtml(): ?string
-    {
-        $html = parent::settingsHtml();
-
-        $html .= Cp::timeFieldHtml([
-            'label' => Craft::t('events', 'Default Time'),
-            'instructions' => Craft::t('events', 'Set a default time for this field for brand-new sessions.'),
-            'id' => 'default-time',
-            'name' => 'defaultTime',
-            'value' => $this->defaultTime ? $this->defaultTime->format('c') : null,
-        ]);
-
-        return $html;
-    }
-
-    protected function inputHtml(ElementInterface $element = null, bool $static = false): ?string
-    {
-        if (!$element instanceof Session) {
-            throw new InvalidArgumentException(static::class . ' can only be used in session field layouts.');
-        }
-
-        // Annoyingly, we need to construct the date/time input ourselves to have a separate date/time default value.
-        $config = [
-            'id' => 'start-date',
-            'name' => 'startDate',
-            'fieldset' => true,
-        ];
-
-        $dateConfig = $config + [
-            'hasOuterContainer' => true,
-            'isDateTime' => true,
-            'value' => $element?->startDate?->format('c') ?? null,
-        ];
-
-        $timeConfig = $config + [
-            'hasOuterContainer' => true,
-            'isDateTime' => true,
-            'outputLocaleParam' => false,
-            'outputTzParam' => false,
-            'value' => $element?->startDate?->format('c') ?? $this->defaultTime?->format('c') ?? null,
-        ];
-
-        return Cp::fieldHtml(Html::tag('div', Cp::dateHtml($dateConfig) . Cp::timeHtml($timeConfig), [
-            'class' => 'datetimewrapper',
-        ]), $config);
     }
 }
