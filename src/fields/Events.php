4da26bb1b0dafdd6ca76d1d7a56c1c61a03ba51f
Josh Crawford <joshua@sgroup.com.au>
2024-10-04 11:44:53 +1000

    Update Events element field to have proper type, GraphQL support and element conditions

--- src/fields/Events.php
+++ src/fields/Events.php
@@ -2,9 +2,18 @@
 namespace verbb\events\fields;
 
 use verbb\events\elements\Event;
+use verbb\events\elements\db\EventQuery;
+use verbb\events\gql\arguments\EventArguments;
+use verbb\events\gql\interfaces\EventInterface;
+use verbb\events\gql\resolvers\EventResolver;
+use verbb\events\helpers\Gql;
 
 use Craft;
+use craft\elements\conditions\ElementCondition;
 use craft\fields\BaseRelationField;
+use craft\models\GqlSchema;
+
+use GraphQL\Type\Definition\Type;
 
 class Events extends BaseRelationField
 {
@@ -30,4 +39,38 @@ class Events extends BaseRelationField
     {
         return Craft::t('events', 'Add an event');
     }
+
+    public static function phpType(): string
+    {
+        return sprintf('\\%s|\\%s<\\%s>', EventQuery::class, ElementCollection::class, Event::class);
+    }
+
+
+    // Public Methods
+    // =========================================================================
+
+    public function includeInGqlSchema(GqlSchema $schema): bool
+    {
+        return Gql::canQueryEvents($schema);
+    }
+
+    public function getContentGqlType(): Type|array
+    {
+        return [
+            'name' => $this->handle,
+            'type' => Type::nonNull(Type::listOf(EventInterface::getType())),
+            'args' => EventArguments::getArguments(),
+            'resolve' => EventResolver::class . '::resolve',
+        ];
+    }
+
+
+    // Protected Methods
+    // =========================================================================
+
+    protected function createSelectionCondition(): ?ElementCondition
+    {
+        return Event::createCondition();
+    }
+
 }
